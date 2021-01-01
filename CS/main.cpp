544c382898d408683ded752bfd88511205cf9fbf
昕睿 阎 <yanxinrui4@gmail.com>
2021-01-02 04:00:20 +0800

    normal process

--- CS/main.cpp
+++ CS/main.cpp
@@ -9,7 +9,7 @@
 #define LENG 20
 using namespace std;
 
-//使用邻接表
+//使用邻接表,分别是飞机的邻接表&逆邻接表，火车的邻接表&逆邻接表
 
 struct city
 {
@@ -18,7 +18,10 @@ struct city
 	shift* adj;
 };
 
-city city_node[MAX];
+city city_node_flight_in[MAX];
+city city_node_flight_out[MAX];
+city city_node_train_in[MAX];
+city city_node_train_in[MAX];
 int city_total_num;				//城市总数
 
 struct shift				//班次信息
@@ -43,14 +46,17 @@ int find_city(char name[])
 void add_city(char name[])
 {
 	int city_num = find_city(name);
-	if (city_num != -1 && city_node[city_num].signal == 1)//已存在
+	if (city_num != -1 && city_node_flight_in[city_num].signal == 1)//已存在
 	{
 		cout << "failed to add!existed!!" << endl;
 		return;
 	}
-	if (city_num != -1 && city_node[city_num].signal == 0)//曾被删除
+	if (city_num != -1 && city_node_flight_in[city_num].signal == 0)//曾被删除
 	{
-		city_node[city_num].signal = 1;
+		city_node_flight_in[city_num].signal = 1;
+		city_node_flight_out[city_num].signal = 1;
+		city_node_train_in[city_num].signal = 1;
+		city_node_train_out[city_num].signal = 1;
 		cout << "succeed to add! The number of city is" << n << endl;
 		return;
 	}
@@ -61,9 +67,18 @@ void add_city(char name[])
 	}
 	else
 	{
-		city_node[city_total_num].city_name = name;
-		city_node[city_total_num].signal = 1;
-		city_node[city_total_num].adj = NULL;
+		city_node_flight_in[city_total_num].city_name = name;
+		city_node_flight_in[city_total_num].signal = 1;
+		city_node_flight_in[city_total_num].adj = NULL;
+		city_node_flight_out[city_total_num].city_name = name;
+		city_node_flight_out[city_total_num].signal = 1;
+		city_node_flight_out[city_total_num].adj = NULL;
+		city_node_train_in[city_total_num].city_name = name;
+		city_node_train_in[city_total_num].signal = 1;
+		city_node_train_in[city_total_num].adj = NULL;
+		city_node_train_out[city_total_num].city_name = name;
+		city_node_train_out[city_total_num].signal = 1;
+		city_node_train_out[city_total_num].adj = NULL;
 		cout << "succeed to add! The number of city is" << city_total_num << endl;
 		city_total_num++;
 		return;
@@ -76,27 +91,47 @@ void delete_city()
 {
 	int city_num = find_city(name);
 	if (city_num == -1)
-	{ 
-		cout << "failed to delete!not exist!" << endl;
-		return;
-	}
-	if (city_num != -1 && city_node[city_num].signal == 0)
 	{
 		cout << "failed to delete!not exist!" << endl;
 		return;
 	}
-	if (city_num != -1 && city_node[city_num].signal == 1)
+	if (city_num != -1 && city_node_flight_in[city_num].signal == 0)
 	{
-		city_node[city_num].signal = 0;
+		cout << "failed to delete!not exist!" << endl;
+		return;
+	}
+	if (city_num != -1 && city_node_flight_in[city_num].signal == 1)
+	{
+		city_node_flight_in[city_num].signal = 0;
+		city_node_flight_out[city_num].signal = 0;
+		city_node_train_in[city_num].signal = 0;
+		city_node_train_out[city_num].signal = 0;
 		cout << "succeed to delete! " << n << endl;
 		return;
 	}
 	
 }
 
-void add_city_info(int )
+void add_city_info(shift info)
 {
+	int start_num; int terminal_num;
+	start_num = find(info.start);
+	terminal_num = find(info.terminal);
+	if (start_num == -1 || (start_num != -1 && city_node_flight_in[start_num].signal == 0))
+	{
+		cout << "fail to add information!start not exist!" << endl;
+		return;
+	}
+	if (terminal_num == -1 || (terminal_num != -1 && city_node_flight_in[terminal_num].signal == 0))
+	{
+		cout << "fail to add information!terminal not exist!" << endl;
+		return;
+	}
+	shift* p;
+	if (info.kind == 1)//flight
+	{
 
+	}
 }
 
 void delete_city_info()
@@ -108,7 +143,27 @@ void delete_city_info()
 void print_schedule(char name[])	//输入城市名称，打印由该城市出发的所有列车路线以及飞机航
 									//班班次信息
 {
+	int city_num = find_city(name);
+	if (city_num == -1)
+	{
+		cout << "failed to print!not exist!" << endl;
+		return;
+	}
+	if (city_num != -1 && city_node_flight_out[city_num].signal == 0)
+	{
+		cout << "failed to print!not exist!" << endl;
+		return;
+	}
+	shift* p;
+	if (city_num != -1 && city_node_flight_out[city_num].signal == 1)
+	{
+		p = city_node_flight_in[city_num].adj;
+		while (p != NULL)
+		{
 
+		}
+		return;
+	}
 	
 }
 
